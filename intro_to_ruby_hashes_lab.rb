67f174930c4b1192970eb8b8ed58e614db9ac581
Coral Fussman <coralfussman@gmail.com>
2020-03-17 18:20:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,10 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+key: = name:
+"value" = "Steve"
 my_hash_creator = {key: "value"}
+
 end
 
 def read_from_hash(hash, key)
